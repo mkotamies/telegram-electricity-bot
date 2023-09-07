23f9ee20e3372e619fef90392ea956e7a5cebb25
Mikko Kotamies <mikko.kotamies@xa.epicgames.com>
2023-09-07 18:18:52 +0300

    fetch prices from porssisahko api

--- electricity.go
+++ electricity.go
@@ -2,8 +2,20 @@ package main
 
 import (
     "fmt"
+    "io/ioutil"
+    "net/http"
 )
 
 func main() {
-    fmt.Println("Hello, World!")
+  resp, err := http.Get("https://api.porssisahko.net/v1/latest-prices.json")
+  if(err != nil) {
+    fmt.Println(err)
+  }
+  defer resp.Body.Close()
+  body, err := ioutil.ReadAll(resp.Body)
+  if(err != nil) {
+    fmt.Println(err)
+  }
+  prices := string(body)
+  fmt.Println(prices)
 }
